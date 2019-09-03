e0f31786d2982234b68df541c5126048baa68613
ddehun <ddehun@kaist.ac.kr>
2019-09-03 19:40:28 +0900

    all script file is end

---
+++ preprocessing.py
@@ -0,0 +1,319 @@
+import struct
+import json
+import argparse
+import os
+from collections import Counter
+from stanfordcorenlp import StanfordCoreNLP as nlp
+from tensorflow.core.example import example_pb2
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--parsed_data_path", type=str, default="data/trainable/split/parsed_perspectrum_data.json")
+parser.add_argument("--processed_data_path", type=str, default="data/trainable/split/processed_perspectrum_data.json")
+parser.add_argument("--split_data_path", type=str, default="data/trainable/split/{}_processed.json")
+parser.add_argument("--data_path", type=str, default="data/trainable/split/train_processed.bin", help="Path to binarized train/valid/test data.")
+parser.add_argument("--vocab_path", type=str, default="data/vocab.txt", help="Path to vocabulary.")
+parser.add_argument("--embed_path", type=str, default="data/embed/glove.6B.300d.txt", help="Path to word embedding.")
+parser.add_argument('--wikitext_raw_path', type=str, default='data/wikitext/wikitext-103/wiki.{}.tokens')
+parser.add_argument('--wikitext_processed_path', type=str, default='data/wikitext/wikitext-103/processed_wiki_{}.txt')
+parser.add_argument("--custom_embed_path", type=str, default="data/embed/my_words.txt")
+parser.add_argument("--model_path", type=str, default="data/log/", help="Path to store the model checkpoints.")
+parser.add_argument("--vocab_size", type=int, default=50000)
+parser.add_argument("--min_cnt", type=int, help="word minimum count", default=1)
+args = parser.parse_args()
+
+
+
+data_path = './data/perspectrum/'
+raw_perspectrum_data = {
+    'claim_pers_fname': data_path+'perspectrum_with_answers_v1.0.json',
+    'pers_pool_fname': data_path+'perspective_pool_v1.0.json',
+    'split_fname': data_path+'dataset_split_v1.0.json'
+}
+
+
+def read_pers_pool(fname):
+    with open(fname, 'r', encoding='utf8') as f:
+        data = json.load(f)
+
+    final_data = dict()
+    for idx, sample in enumerate(data):
+        pId, text = sample['pId'], sample['text']
+        final_data[pId] = text
+    return final_data
+
+
+def read_claim_pers(fname):
+    with open(fname, 'r', encoding='utf8') as f:
+        data = json.load(f)
+
+    final_data = dict()
+    for idx,sample in enumerate(data):
+        cId, text, pers = sample['cId'], sample['text'], sample['perspectives']
+        final_data[cId] = {
+            'text': text,
+            'pers': pers
+        }
+    return final_data
+
+
+def match_claim_pers(claims, pers_texts):
+    for cId in claims:
+        text_pers = {}
+        pers = claims[cId]['pers']
+        for per in pers:
+            one_per_texts = []
+            pids = per['pids']
+            for pid in pids:
+                one_per_texts.append(pers_texts[pid])
+            text_pers[len(list(text_pers.keys()))] = one_per_texts
+        claims[cId]['pers'] = text_pers
+    return claims
+
+
+def make_merged_dataset():
+    """
+    Read claim and perspective file & merge into one file.
+    """
+    if os.path.exists(args.parsed_data_path):
+        with open(args.parsed_data_path, 'r', encoding='utf8') as f:
+            return json.load(f)
+
+    pers = read_pers_pool(raw_perspectrum_data['pers_pool_fname'])
+    claims = read_claim_pers(raw_perspectrum_data['claim_pers_fname'])
+    matched = match_claim_pers(claims, pers)
+    with open(args.parsed_data_path, 'w', encoding='utf8') as f:
+        json.dump(matched, f)
+    return matched
+
+
+def make_processed_dataset(raw_dataset):
+    assert os.path.exists(args.parsed_data_path)
+    if os.path.exists(args.processed_data_path):
+        with open(args.processed_data_path, 'r', encoding='utf8') as f:
+            return json.load(f)
+    processor = Preprocessor()
+
+    for cid, item in raw_dataset.items():
+        assert 'pers' in item and 'text' in item
+        text_tok = processor.preprocess(item['text'])
+
+        processed_pers = {}
+        for perId in item['pers']:
+            processed_per = []
+            for one_per in item['pers'][perId]:
+                assert isinstance(one_per, str)
+                tokens = processor.preprocess(one_per)
+                processed_per.append(tokens)
+            processed_per = {k: v for k, v in enumerate(processed_per)}
+            processed_pers[len(list(processed_pers.keys()))] = processed_per
+        raw_dataset[cid]['text'] = text_tok
+        raw_dataset[cid]['pers'] = processed_pers
+
+    with open(args.processed_data_path, 'w', encoding='utf8') as f:
+        json.dump(raw_dataset, f)
+    return raw_dataset
+
+
+class Preprocessor:
+    def __init__(self):
+        self.nlp = nlp(args.parser_path)
+
+    def preprocess(self, text):
+        assert isinstance(text, str)
+        tokens = self.tokenize(text)
+        return tokens
+
+    def tokenize(self, text):
+        res = [tok.lower() for tok in self.nlp.word_tokenize(text.replace('-', ' '))]
+        return res
+
+
+def split_dataset(processed_dataset):
+    """
+    Split the preprocessed dataset into train/valid/test.
+    """
+    with open(raw_perspectrum_data['split_fname'], 'r', encoding='utf8') as f:
+        split_guide = json.load(f)
+
+    train, dev, test = {}, {}, {}
+
+    for cId, item in processed_dataset.items():
+        assert cId in split_guide
+        setname = split_guide[cId]
+        if setname == 'train':
+            train[cId] = item
+        elif setname == 'dev':
+            dev[cId] = item
+        elif setname == 'test':
+            test[cId] = item
+        else:
+            raise KeyError
+
+    with open(args.split_data_path.format('train'), 'w', encoding='utf8') as f:
+        json.dump(train, f)
+    with open(args.split_data_path.format('dev'), 'w', encoding='utf8') as f:
+        json.dump(dev, f)
+    with open(args.split_data_path.format('test'), 'w', encoding='utf8') as f:
+        json.dump(test, f)
+    return train, dev, test
+
+
+def build_vocab():
+    assert os.path.exists(args.split_data_path.format('train'))
+
+    if os.path.exists(args.vocab_path):
+        with open(args.vocab_path, 'r') as f:
+            ls = f.readlines()
+            vocab = [line.strip() for line in ls]
+            vocab = vocab[:args.vocab_size]
+            print(str(len(vocab)) + ' words in vocab.')
+            return vocab
+
+    with open(args.split_data_path.format('train'), 'r', encoding='utf8') as f:
+        data1 = json.load(f)
+    with open(args.split_data_path.format('dev'), 'r', encoding='utf8') as f:
+        data2 = json.load(f)
+    with open(args.split_data_path.format('test'), 'r', encoding='utf8') as f:
+        data3 = json.load(f)
+    data = dict(data1, **data2)
+    data = dict(data, **data3)
+
+    counter = Counter()
+
+    for cid,item in data.items():
+        text, pers = item['text'], item['pers']
+
+        all_tokens = []
+        for perid, per in pers.items():
+            for pid, pertoks in per.items():
+                assert isinstance(pertoks, list) and all([isinstance(tok, str) for tok in pertoks])
+                all_tokens.extend(pertoks)
+
+        assert all([isinstance(tok, str) for tok in all_tokens])
+        counter.update(text + all_tokens)
+
+    vocab = ['<BEG>', '<PAD>', '<EOS>', '<UNK>']
+    vocab += [el[0] for el in counter.most_common() if el[1] >= args.min_cnt]
+    vocab = vocab[:args.vocab_size]
+    print("Total unique token in train: {}".format(len(vocab)))
+
+    counter = Counter()
+    """
+    Count the Wikitext tokens first.
+    """
+    with open(args.wikitext_processed_path.format('train'), 'r', encoding='utf8') as f:
+        ls = f.readlines()
+    for line in ls:
+        tokens = [tok for tok in line.strip().split() if tok != '<unk>']
+        counter.update(tokens)
+    wiki_words = [el[0] for el in counter.most_common()]
+    print("Wiki tokens: {}".format(len(wiki_words)))
+
+    """
+    Consider original dataset vocab. 
+    """
+    while len(vocab) < args.vocab_size and len(wiki_words) != 0:
+        wiki_tok = wiki_words.pop(0)
+        if wiki_tok not in vocab:
+            vocab.append(wiki_tok)
+    print('Final vocab size: {}'.format(len(vocab)))
+
+    with open(args.vocab_path, 'w') as f:
+        f.write('\n'.join(vocab))
+    return vocab
+
+
+def process_wikitext(setname):
+    assert os.path.exists(args.wikitext_raw_path.format(setname))
+    if os.path.exists(args.wikitext_processed_path.format(setname)): return
+    with open(args.wikitext_raw_path.format(setname), 'r', encoding='utf8') as f:
+        ls = f.readlines()
+    processed_wiki = []
+    for line in ls:
+        if len(line.strip()) == 0 or '=' in line: continue
+        line = line.strip().replace('@-@', '-').replace('@.@', '.').replace('@,@', ',').lower()
+        processed_wiki.append(line)
+    with open(args.wikitext_processed_path.format(setname), 'w', encoding='utf8') as f:
+        f.write('\n'.join(processed_wiki))
+
+
+def create_wikitext_bin_file(setname):
+    processed_fname = args.wikitext_processed_path.format(setname)
+    bin_fname = processed_fname.replace('.txt', '.bin')
+    assert os.path.exists(processed_fname)
+    if os.path.exists(bin_fname): return
+
+    with open(processed_fname, 'r', encoding='utf8') as f:
+        ls = [line.strip() for line in f.readlines()]
+
+    with open(bin_fname, 'wb') as f:
+        for line in ls:
+            example = example_pb2.Example()
+            example.features.feature['text'].bytes_list.value.extend([line.encode()])
+            example_str = example.SerializeToString()
+            str_len = len(example_str)
+            f.write(struct.pack('q', str_len))
+            f.write(struct.pack('%ds' % str_len, example_str))
+
+
+def create_bin_file(setname):
+    split_fname = args.split_data_path.format(setname)
+    bin_fname = args.split_data_path.format(setname).replace('.json', '.bin')
+
+    if os.path.exists(bin_fname): return
+    with open(split_fname, 'r', encoding='utf8') as f:
+        split_data = json.load(f)
+
+    enc_dat, dec_dat, cids, pids, ppids = [[] for _ in range(5)]
+
+    for k, v in split_data.items():
+        cid, enc_text = k, v['text']
+
+        for pid, per in v['pers'].items():
+            for ppid, pptext in per.items():
+                assert isinstance(int(pid), int) and isinstance(int(ppid), int) and isinstance(pptext, list) and all([isinstance(tok, str) for tok in pptext])
+                enc_dat.append(enc_text)
+                dec_dat.append(pptext)
+                cids.append(k)
+                pids.append(pid)
+                ppids.append(ppid)
+
+    assert len(enc_dat) == len(dec_dat) == len(cids) == len(pids) == len(ppids)
+    import random
+    idx_list = random.sample([_ for _ in range(len(ppids))], len(ppids))
+    assert len(idx_list) == len(list(set(idx_list)))
+    enc_dat = [enc_dat[idx] for idx in idx_list]
+    dec_dat = [dec_dat[idx] for idx in idx_list]
+    cids = [cids[idx] for idx in idx_list]
+    pids = [pids[idx] for idx in idx_list]
+    ppids = [ppids[idx] for idx in idx_list]
+
+    with open(bin_fname, 'wb') as f:
+        for idx in range(len(enc_dat)):
+            enc_text, dec_text, cid, pid, ppid = ' '.join(enc_dat[idx]), ' '.join(dec_dat[idx]), cids[idx], pids[idx], ppids[idx]
+            example = example_pb2.Example()
+            example.features.feature['enc'].bytes_list.value.extend([enc_text.encode()])
+            example.features.feature['dec'].bytes_list.value.extend([dec_text.encode()])
+            example.features.feature['cid'].bytes_list.value.extend([cid.encode()])
+            example.features.feature['pid'].bytes_list.value.extend([pid.encode()])
+            example.features.feature['ppid'].bytes_list.value.extend([ppid.encode()])
+            example_str = example.SerializeToString()
+            str_len = len(example_str)
+            f.write(struct.pack('q', str_len))
+            f.write(struct.pack('%ds' % str_len, example_str))
+
+
+if __name__ == '__main__':
+    raw_dataset = make_merged_dataset()
+    processed_dataset = make_processed_dataset(raw_dataset)
+    train, dev, test = split_dataset(processed_dataset)
+    for setname in ['train', 'valid', 'test']:
+        process_wikitext(setname)
+
+    build_vocab()
+
+    for setname in ['train', 'dev', 'test']:
+        create_bin_file(setname)
+    for setname in ['train', 'valid', 'test']:
+        create_wikitext_bin_file(setname)
---
+++ utils.py
@@ -0,0 +1,167 @@
+import os
+import tensorflow as tf
+from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
+
+
+def jaccard_distance(text1, text2):
+    """ Measure the jaccard distance of two different text.
+    ARGS:
+        text1,2: list of tokens
+    RETURN:
+        score(float): distance between two text
+        """
+    intersection = set(text1).intersection(set(text2))
+    union = set(text1).union(set(text2))
+    return 1 - len(intersection) / len(union)
+
+
+def load_ckpt(args, saver, sess, ckpt_dir='train', ckpt_id=None):
+    while True:
+        if ckpt_id is None or ckpt_id == -1:
+            ckpt_dir = os.path.join(args.model_path, ckpt_dir)
+            ckpt_state = tf.train.get_checkpoint_state(ckpt_dir, latest_filename=None)
+            print(ckpt_dir)
+            ckpt_path = ckpt_state.model_checkpoint_path
+            print("CKPT_PATH: {}".format(ckpt_path))
+            # print_tensors_in_checkpoint_file(file_name=ckpt_path, tensor_name='', all_tensors=False)
+            saver.restore(sess, ckpt_path)
+            return ckpt_path
+
+
+def gpu_config():
+    config = tf.ConfigProto(allow_soft_placement=True)
+    config.gpu_options.allow_growth = True
+    return config
+
+
+def assign_specific_gpu(gpu_nums='-1'):
+    assert gpu_nums is not None and gpu_nums != '-1'
+    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+    os.environ['CUDA_VISIBLE_DEVICES'] = gpu_nums
+
+
+class Vocab():
+    def __init__(self, path='data/vocab.txt'):
+        self.word2id, self.id2word = {}, {}
+        self.vocabpath = path
+        self.read_voca()
+
+    def read_voca(self):
+        assert os.path.exists(self.vocabpath)
+        with open(self.vocabpath, 'r', encoding='utf8') as f:
+            ls = [line.strip() for line in f.readlines()]
+        for idx, word in enumerate(ls):
+            self.word2id[word] = idx
+            self.id2word[idx] = word
+        self.unk_id = self.word2id['<UNK>']
+        self.beg_id = self.word2id['<BEG>']
+        self.eos_id = self.word2id['<EOS>']
+        self.pad_id = self.word2id['<PAD>']
+        self.words = list(self.word2id.keys())
+        self.word_sorted = ls
+
+    def text2ids(self, toks):
+        assert isinstance(toks, list) and all([isinstance(tok, str) for tok in toks])
+        ids = [self.word2id[tok] if tok in self.word2id else self.unk_id for tok in toks]
+        return ids
+
+
+def get_pretrain_weights(path):
+    """ Load pretrain weights and save """
+    with tf.Session(config=gpu_config()) as sess:
+        ckpt_name = tf.train.latest_checkpoint(path)
+        meta_name = ckpt_name + '.meta'
+        save_graph = tf.train.import_meta_graph(meta_name)
+        save_graph.restore(sess, ckpt_name)
+        var_name_list = [var.name for var in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)]
+        vardict = dict()
+        for var in var_name_list:
+            vardict[var] = sess.run(tf.get_default_graph().get_tensor_by_name(var))
+    tf.reset_default_graph()
+    return vardict
+
+
+def print_config(args):
+    print('mode: {}'.format(args.mode))
+    print('model: {}'.format(args.model))
+    print("use pretrain: {}".format(args.use_pretrain))
+    print("Batch size: {}".format(args.batch_size))
+
+
+def assign_pretrain_weights(pretrain_vardicts):
+    assign_op, uninitialized_varlist = [], []
+    all_variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+    assign_op_names = []
+
+    for var in all_variables:
+        varname = var.name
+        new_model_var = tf.get_default_graph().get_tensor_by_name(varname)
+        if 'bw_model' in varname: varname = varname.replace('bw_model', 'model')
+        if varname in pretrain_vardicts:
+            assign_op.append(tf.assign(new_model_var, pretrain_vardicts[varname]))
+            assign_op_names.append(varname)
+        else:
+            if varname.replace('model/decoder/attention_decoder', 'model/decoder/rnn') in pretrain_vardicts:
+                corres_varname = varname.replace('model/decoder/attention_decoder', 'model/decoder/rnn')
+                assign_op.append(tf.assign(new_model_var, pretrain_vardicts[corres_varname]))
+                assign_op_names.append(varname)
+            elif varname.replace('model/tgt_encoder/', 'model/encoder/') in pretrain_vardicts:
+                corres_varname = varname.replace('model/tgt_encoder/', 'model/encoder/')
+                assign_op.append(tf.assign(new_model_var, pretrain_vardicts[corres_varname]))
+                assign_op_names.append(varname)
+            elif ('/encoder/' in varname or '/decoder/' in varname) and ('kernel:0' in varname or 'bias:0' in varname):
+                raise ValueError("{} should be pretrained.".format(varname))
+            else:
+                uninitialized_varlist.append(var)
+
+    return assign_op, uninitialized_varlist
+
+
+def ids2tokens(ids, vocab):
+    toks = [vocab.id2word[id_] for id_ in ids if id_ not in [vocab.pad_id, vocab.eos_id]]
+    return toks
+
+
+def sample_gumbel(shape, eps=1e-10):
+  """Sample from Gumbel(0, 1)"""
+  U = tf.random_uniform(shape, minval=0.0, maxval=1.0)
+  tf.stop_gradient(U)
+  return -tf.log(-tf.log(U + eps) + eps)
+
+
+def gumbel_softmax_sample(logits, temp):
+  """ Draw a sample from the Gumbel-Softmax distribution"""
+  y = logits + sample_gumbel(tf.shape(logits))
+  return tf.nn.softmax(y / temp)
+
+
+def gumbel_softmax(logits, temp, hard=False):
+  """Sample from the Gumbel-Softmax distribution and optionally discretize.
+  Args:
+    logits: [batch_size, n_class] unnormalized log-probs
+    temperature: non-negative scalar
+    hard: if True, take argmax, but differentiate w.r.t. soft sample y
+  Returns:
+    [batch_size, n_class] sample from the Gumbel-Softmax distribution.
+    If hard=True, then the returned sample will be one-hot, otherwise it will
+    be a probabilitiy distribution that sums to 1 across classes
+  """
+  y = gumbel_softmax_sample(logits, temp)
+  if hard:
+    k = tf.shape(logits)[-1]
+    #y_hard = tf.cast(tf.one_hot(tf.argmax(y,1),k), y.dtype)
+    y_hard = tf.cast(tf.equal(y, tf.reduce_max(y, 1, keep_dims=True)), y.dtype)
+    y = tf.stop_gradient(y_hard - y) + y
+  return y
+
+
+if __name__ == '__main__':
+    logit = tf.Variable([[2,2,3,1,4,1,1,2,1,1]],dtype=tf.float32)
+
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        logit = tf.nn.softmax(logit)
+        res2 = gumbel_softmax(logit, temp=0.67, hard=False)
+        print(sess.run(res2))
+
